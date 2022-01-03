ef1461174a07e81722f5c19986c500990b3fc743
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-03 12:51:43 +0100

    hot fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list,idx,element):
     new = list(my_list)
     if idx < 0:
         return new
-    elif idx < len(my_list) - 1:
+    elif idx > len(my_list) - 1:
         return new
     else:
         new[idx] = element

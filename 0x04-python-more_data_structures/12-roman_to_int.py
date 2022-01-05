1df7c6fec38bed029389d40956686425651f9a36
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-05 13:42:20 +0100

    hot fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str) is False:
-        return 0
-    elif not roman_string:
+    if not isinstance(roman_string, str):
         return 0
     else:
         dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,

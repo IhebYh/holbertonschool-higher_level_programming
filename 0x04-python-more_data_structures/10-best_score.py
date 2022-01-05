8853d6c27452e74eb7d4ee2fd86188471e859631
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-05 13:09:07 +0100

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    kv = 0
+    k = ""
+    for i in a_dictionary:
+        if a_dictionary[i] > kv:
+            k = i
+            kv = a_dictionary[i]
+    return k

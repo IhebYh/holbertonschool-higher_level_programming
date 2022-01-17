1370d36c475de197f03161416c2428a13ccd54c4
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-17 14:08:10 +0100

    hot fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
     prints x element of a list
+
 """
 
 
@@ -8,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
     nb = 0
     for i in range(0, x):
         try:
-            print("{:d}".format(i), end="")
+            print(my_list[i], end="")
             nb += 1
         except:
             break

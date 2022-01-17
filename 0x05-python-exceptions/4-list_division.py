a23252d5fc7d62ecd24a868d76c80e2826ea4efa
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-17 14:44:03 +0100

    print a division list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    prints list division
+
+    """
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    res = []
+    for x in range(0, list_length):
+        try:
+            res.append(my_list_1[x] / my_list_2[x])
+        except IndexError:
+            print("out of range")
+            res.append(0)
+        except TypeError:
+            print("wrong type")
+            res.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            res.append(0)
+        finally:
+            pass
+    return res

1bad9314efc4e088830e8679b25ae53ba590ad64
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-18 11:58:25 +0100

    init class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module Square"""
+
+
+class Square:
+    """Square class defined by geometric shape
+
+        Attributes:
+            size(int): size of square
+
+    """
+    def __init__(self, size):
+        """
+        Initialize methode
+
+        Args:
+            size (int): size of square
+        """
+        self.__size = size

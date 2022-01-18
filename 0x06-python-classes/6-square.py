c360f5b6725e9ce6e034ef740c68e375305ae138
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-18 23:22:16 +0100

    fixedd

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,7 +18,7 @@ class Square:
             None
         """
         self.__size = size
-        self.__position = position
+        self.position = position
 
     def area(self):
         """
@@ -69,10 +69,10 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            print('\n'*self.__position[1], end="")
+            print('\n' * self.position[1], end="")
             for i in range(self.size):
-                print(" "*self.__position[0], end="")
-                print("#"*self.size)
+                print(" " * self.position[0], end="")
+                print("#" * self.size)
 
     @property
     def position(self):

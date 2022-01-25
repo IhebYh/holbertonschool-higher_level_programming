7012c92caf4e7c16f59ca85952fcac2330af1b36
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-25 12:47:29 +0100

    N/A

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Unittest for max_integer([])
+"""
+
+import unittest
+
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """ tests for max_integer"""
+    def test_normal(self):
+        """ test normal input"""
+        self.assertEqual(max_integer([1, 2, 3, 4], 4))
+
+    def test_no_arg(self):
+        """tests empty input"""
+        self.assertEqual(max_integer(), None)
+
+    def test_empty_list(self):
+        """tests empty list"""
+        self.assertEqual(max_integer([]), None)
+
+    def test_identical(self):
+        """test identical input"""
+        self.assertEqual(max_integer([1, 1, 1], 1))
+
+    def test_unordered(self):
+        """ tests unordered input"""
+        self.assertEqual(max_integer([4, 8, 2, 7], 8))
+
+    def test_positive_negative(self):
+        """ tests positive/negative input"""
+        self.assertEqual(max_integer([-32, 2, -8, 10, -1]), 10)
+
+    def test_negative(self):
+        """ tests negative input"""
+        self.assertEqual(max_integer([-32, -2, -8, -10, -1]), -1)
+
+    def test_float(self):
+        """ tests float input"""
+        self.assertEqual(max_integer(
+                                    [-32.0001, -32.001,
+                                     2.885, -8.058,
+                                     10.5569, 1.058]), 10.5569)
+
+    def test_numeric_str(self):
+        """ tests positive/negative input"""
+        self.assertEqual(max_integer("198565468"), "9")
+
+    def test_string(self):
+        self.assertEqual(max_integer("IhebYh", "h"))

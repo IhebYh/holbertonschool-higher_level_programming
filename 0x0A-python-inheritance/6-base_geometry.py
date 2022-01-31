2cfce6e4ae100d6524c10a67895e58655aa8bc87
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-31 13:29:12 +0100

    raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+base geometry module
+"""
+
+
+class BaseGeometry():
+    """
+    base geomtry class
+    """
+    def area(self):
+        """
+        public instance
+        Raises:
+            exception: area not implemented
+        """
+        raise Exception("area() is not implemented")

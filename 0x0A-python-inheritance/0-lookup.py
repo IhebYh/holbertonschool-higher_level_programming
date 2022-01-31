660908030c64e4bf77926faaf28bc8618f8199d2
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-31 13:07:15 +0100

    project init + first task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Lookup module
+"""
+
+
+def lookup(obj):
+    """
+    a function that returns the list of available attributes and meth of an obj
+    """
+    return (dir(obj))

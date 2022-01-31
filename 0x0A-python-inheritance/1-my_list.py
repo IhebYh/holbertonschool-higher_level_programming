61218dc708ffbfdf63eb0840619758a768db3b04
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-31 13:10:38 +0100

    print sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+MyList module inherited from list
+"""
+
+
+class MyList(list):
+    """
+    MyList class
+    """
+    def print_sorted(self):
+        """
+        print a sorted list
+        """
+        return (sorted(slef))

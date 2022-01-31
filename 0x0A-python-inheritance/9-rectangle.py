fdc9f293bf990babd636a5bdd878e8ac1ab51d05
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-31 13:45:23 +0100

    area + str + typo fix

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,6 +2,8 @@
 """
 Rectangle module
 """
+
+
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
@@ -17,3 +19,15 @@ class Rectangle(BaseGeometry):
         self.integer_validator('height', height)
         self.__width = width
         self.__height = height
+    
+    def area(self):
+        """
+        area of a rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+            Rect Description
+        """
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))

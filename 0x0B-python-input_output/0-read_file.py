f7ac3682f0c399f04aaa6c7dcb3e448527bc06ea
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-02-01 12:03:56 +0100

    project init + first task

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+read file module
+"""
+
+
+def read_file(filename=""):
+    """
+    reading a text file (UTF8) and prints it to stdout
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")

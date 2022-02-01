f28875b400f6e536abedc36c63a191ef8eaa6189
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-02-01 12:32:30 +0100

    load from json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+load from json file module
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    creates an object from "JSON file"
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)

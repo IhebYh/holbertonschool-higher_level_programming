fa0a3cc85ef8c6b0a5055f3c9382a69289f893a5
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-02-04 13:19:25 +0100

    repo init + 1st and 2nd task

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Base class module
+"""
+
+
+class Base:
+    """
+    Base class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        base instance init
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

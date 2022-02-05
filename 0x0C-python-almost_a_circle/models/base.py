a2388bf67893461989f3d4d3326a87a0f917b76e
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-02-05 13:18:23 +0100

    task 19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Base class module
 """
+import json
 
 
 class Base:
@@ -19,3 +20,51 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of a list of dictionaries"""
+        if list_dictionaries is None:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        lo = []
+        if list_objs is not None:
+            for i in list_objs:
+                lo.append(cls.to_dictionary(i))
+        with open(filename, 'w') as f:
+            f.write(cls.to_json_string(lo))
+
+    @classmethod
+    def create(cls, **dictionnary):
+        """ returns an instance with all attributes already set"""
+        if cls.__name__ is "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ is "Square":
+            dummy = cls(1)
+        dummy.update(**dictionnary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        fname = cls.__name__ + ".json"
+        list = []
+        try:
+            with open(fname, 'r') as f:
+                list = cls.from_json_string(f.read())
+            for i, j in enumerate(list):
+                list[i] = cls.create(**list[i])
+        except:
+            pass
+        return list

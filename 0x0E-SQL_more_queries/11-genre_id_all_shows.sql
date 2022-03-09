8ae597f550d9ad82505b0b237a4b16d55cd83893
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-03-09 23:31:24 +0100

    fixed

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,8 @@
 --lists all shows contained in the database
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title, 
+CASE ISNUMERIC(tv_show_genres.genre_id)  THEN tv_show_genres.genre_id
+ELSE Null
+END
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id

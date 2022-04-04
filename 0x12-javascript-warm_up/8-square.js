fce6baa1409226d0d124b5a1366104178aa5b48c
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-04-04 16:24:57 +0100

    fixed -> error msg while size missed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (isNaN(process.argv[2])) {
-  console.log('Missing number of occurences');
+  console.log('Missing size');
 } else {
   const x = parseInt(process.argv[2]);
   for (let i = 0; i < x; i++) {

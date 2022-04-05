1113d4795af2f547adb78ac8c3cde6cfd515c7b4
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-04-05 16:24:45 +0100

    reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let rev;
-  for (let i = list.length - 1; i > 0; i--) { rev.append(list[i]); }
+  const rev = [];
+  for (let i = list.length - 1; i > 0; i--) { rev.push(list[i]); }
   return rev;
 };

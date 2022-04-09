996a2b81c3ee5bcad1e7f2918a54b2fbbe46d12c
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-04-09 16:37:29 +0100

    filter safe from sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+filtering states safe from sql injection
+"""
+import sys
+import MySQLdb
+
+
+def main():
+    cn = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
+    cursor = cn.cursor()
+    safeSearch = sys.agrv[4]
+    cursor.execute("""SELECT * FROM states
+                    WHERE name={:s}
+                    ORDER BY id ASC""", (safeSearch,))
+    res = cursor.fetchall()
+    for r in res:
+        if r[1][0] == 'N':
+            print(r)
+    cursor.close()
+    cn.close()
+
+
+if __name__ == "__main__":
+    main()

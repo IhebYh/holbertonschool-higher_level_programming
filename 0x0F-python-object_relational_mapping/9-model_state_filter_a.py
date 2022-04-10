b5a61a73cb3473a5050cf4e069d4d3d2bfa4e1dc
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-04-10 15:26:18 +0100

    filtered query

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,7 +13,8 @@ if __name__ == '__main__':
         'mysql+mysqldb://{}:{}@localhost/{}'
         .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     session = Session(engine)
-    for state in session.query(State).order_by(State.id).all():
-       if state.name.contains('a'):
-            print("{}: {}".format(state.id, state.name))
+    m = '%a%'
+    query = session.query(State).filter(State.name.like(m)).order_by(State.id)
+    for q in query:
+        print("{}: {}".format(state.id, state.name))
     session.close()

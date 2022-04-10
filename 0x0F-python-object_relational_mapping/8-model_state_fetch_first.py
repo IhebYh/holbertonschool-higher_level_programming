9c2fac2f97581ec87a5f86b1c045ffcb198b42ed
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-04-10 15:14:37 +0100

    fetch first

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,6 +13,9 @@ if __name__ == '__main__':
         'mysql+mysqldb://{}:{}@localhost/{}'
         .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     session = Session(engine)
-    for state in session.query(State).order_by(State.id).one():
-        print("{}: {}".format(state.id, state.name))
+    re = session.query(State).order_by(State.id).first()
+    if re is None:
+        print("Nothing")        
+    else:
+        print("{}: {}".format(re.id, re.name))
     session.close()

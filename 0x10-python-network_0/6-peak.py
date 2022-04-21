d5d47375412b81a50389d9a244accc9447998c68
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-04-22 00:21:19 +0100

    fixed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,12 +4,13 @@ def find_peak(list_of_integers):
     find peak module
     """
     li = list_of_integers
-    l = len(li)
-    if l == 0:
+    length = len(li)
+    if length == 0:
         return
-    m = l // 2
-    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
+    m = length // 2
+    if (m == length - 1 or li[m] >= li[m + 1])./
+    and (m == 0 or li[m] >= li[m - 1]):
         return li[m]
-    if m != l - 1 and li[m + 1] > li[m]:
+    if m != length - 1 and li[m + 1] > li[m]:
         return find_peak(li[m + 1:])
     return find_peak(li[:m])

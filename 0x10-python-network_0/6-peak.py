9b7c2517c0282d33438df29fdaccf3bee1c121f5
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-04-22 00:07:22 +0100

    fixed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,14 +3,5 @@ def find_peak(a):
     """
     find peak module
     """
-    if a == []:
-        return None
-    if len(a) == 1:
-        return a[0]
-    if a[0] >= a[1]:
-        return a[0]
-    if a[len(a) - 1] >= a[len(a) - 2]:
-        return a[len(a) - 1]
-    for i in range(1, len(a) - 1):
-        if a[i] >= a[i - 1] and a[i] >= a[i + 1]:
-            return a[i]
+    a.sort()
+    return a[-1:]

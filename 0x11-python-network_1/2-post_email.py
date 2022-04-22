b73977bf8f7b6fc19a9f0ccef8f38738476ab7c8
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-04-22 17:30:57 +0100

    2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Post an email to an url
+"""
+import email
+from sys import argv
+from urllib import request, parse
+
+if __name__ == "__main__":
+    value = {"email": argv[2]}
+    data = parse.urlencode(value)
+    data = data.encode("ascii")
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as res:
+        info = res.read()
+    print(info.decode("ascii"))

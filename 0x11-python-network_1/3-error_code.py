df00f53fd153bd51e04033e317df41cc85932314
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-04-22 17:46:41 +0100

    3. Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,10 +6,7 @@ from sys import argv
 from urllib import request, parse, error
 
 if __name__ == "__main__":
-    value = {"email": argv[2]}
-    data = parse.urlencode(value)
-    data = data.encode("ascii")
-    req = request.Request(argv[1], data)
+    req = request.Request(argv[1])
     try:
         with request.urlopen(req) as res:
             info = res.read()

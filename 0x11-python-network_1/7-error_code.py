4fd995368813986207594924d3f577fc4539d768
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-04-24 21:04:17 +0100

    fixed done

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,7 +5,7 @@ requests model
 if __name__ == '__main__':
     import requests
     from sys import argv
-    r = requests.post(argv[1])
+    r = requests.get(argv[1])
     if r.status_code >= 400:
         print("Error code:{}".format(r.status_code))
     else:

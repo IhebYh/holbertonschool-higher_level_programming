f4d3ac41daa3b6cb56fad356defefc4ba5a58a77
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-05-18 22:54:44 +0100

    get + filter

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const req = require('request');
+const url = process.argv[2];
+req.get(url, function (err, response, body) {
+  let sum = 0;
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.parse(body);
+  for (let i = 0; data.results[i] !== undefined; ++i) {
+    if (data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+      sum++;
+    }
+  }
+  console.log(sum);
+});
